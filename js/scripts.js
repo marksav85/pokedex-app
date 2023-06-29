9755e7fe3998f1e0cc75bb86f6ca77131823ca06
Mark Saville <mark.saville@outlook.com>
2023-06-29 15:33:36 +0200

    create array of objects describing pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favouriteFood = 'pizza';
-document.write(favouriteFood);
+let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
+                   {name: 'Pikachu', height: 0.4, types: ['electric']}, 
+                   {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, 
+                   {name: 'Squirtle', height: 0.5, types: ['water']}
+                    ];
+document.write(pokemonList[0].name);
