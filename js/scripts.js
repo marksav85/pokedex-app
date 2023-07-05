6ae7e50c417b95b01ff37d4d13212e4a8faf0add
Mark Saville <mark.saville@outlook.com>
2023-07-05 19:18:12 +0200

    add function to check validity of new keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-/* let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
-                   {name: 'Pikachu', height: 0.4, types: ['electric']}, 
-                   {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, 
-                   {name: 'Squirtle', height: 0.5, types: ['water']}
-                    ]; */
-
 let pokemonRepository = (function () {
   let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
                    {name: 'Pikachu', height: 0.4, types: ['electric']}, 
@@ -15,21 +9,34 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  
   function add(pokemon) {
-    if (typeof pokemon !== 'object'){
+    let objectCount = 0; // counter to ensure position of keyname in object is in correct order
+    let validObject = true; // if true then keynames valid
+    if (typeof pokemon !== 'object'){ // if not an object then invalid
       console.log('input is not an object');
-      } else if (Object.keys(pokemon) == 'name' || Object.keys(pokemon) == 'height' || Object.keys(pokemon) == 'types' && Object.keys(pokemon)) { 
-          console.log('object successfully added')
-          pokemonList.push(pokemon);
-        } else {
-          console.log('object keyname is invalid');
-          }
+    } else if (Object.keys(pokemon).forEach(function(input) { // loop to check all keynames and positions are correct and in order
+              if ((input === 'name' && objectCount == 0) || (input === 'height' && objectCount == 1) || (input === 'types' && objectCount == 2)) { 
+                    objectCount += 1; 
+                    validObject = true;
+
+              } else {
+                      console.log('invalid input'); // if keyname and/or position is false then invalid input
+                      validObject = false;
+              }
+    })); 
+
+    if (validObject == true) { // if object is valid then pushed to repository
+      console.log('object successfully added to repository');
+      pokemonList.push(pokemon);
+      };
   };
       
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
   };
+
 })();
 
 let pokeList = pokemonRepository.getAll()
