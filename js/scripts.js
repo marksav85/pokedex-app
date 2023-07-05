d75aeede1aea34014296d2c560be00fce00a150b
Mark Saville <mark.saville@outlook.com>
2023-07-05 11:30:10 +0200

    create function to return and add to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,38 @@
-let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
+/* let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
+                   {name: 'Pikachu', height: 0.4, types: ['electric']}, 
+                   {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, 
+                   {name: 'Squirtle', height: 0.5, types: ['water']}
+                    ]; */
+
+let pokemonRepository = (function () {
+  let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
                    {name: 'Pikachu', height: 0.4, types: ['electric']}, 
                    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, 
                    {name: 'Squirtle', height: 0.5, types: ['water']}
                     ];
 
+  function getAll() {
+    return pokemonList;
+  }
 
-pokemonList.forEach(function(pokemon) {
-    if (pokemon.height > 0.6) {
-        // adds extra span string if height above 0.6
-        document.write ('<p>'), document.write(pokemon.name + ' (height: ' + pokemon.height + ') '), document.write('<span> - Wow, that\'s big!</span>'), document.write('</p>');
-      } else {
-        // otherwise just prints name and height
-        document.write ('<p>'), document.write (pokemon.name + ' (height: ' + pokemon.height + ') '), document.write('</p>');
-      }
-    });
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+let pokeList = pokemonRepository.getAll()
+
+pokeList.forEach(function(pokemon) {
+  if (pokemon.height > 0.6) {
+      // adds extra span string if height above 0.6
+      document.write ('<p>'), document.write(pokemon.name + ' (height: ' + pokemon.height + ') '), document.write('<span> - Wow, that\'s big!</span>'), document.write('</p>');
+    } else {
+      // otherwise just prints name and height
+      document.write ('<p>'), document.write (pokemon.name + ' (height: ' + pokemon.height + ') '), document.write('</p>');
+    }
+  });
