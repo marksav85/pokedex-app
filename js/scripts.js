4388c4930de092cb81f92710793fc01973e30adc
Mark Saville <mark.saville@outlook.com>
2023-07-10 18:41:00 +0200

    remove pokemon from pokemonl list

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 let pokemonRepository = (function () {
-  let pokemonList = [{name: 'Charmander', height: 0.6, types: ['fire']},
-                   {name: 'Pikachu', height: 0.4, types: ['electric']}, 
-                   {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, 
-                   {name: 'Squirtle', height: 0.5, types: ['water']}
-                    ];
+  let pokemonList = [];
 
   function getAll() {
     return pokemonList;
