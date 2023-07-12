586027ea1f7a623bdf6ccf0f74031d7f8573321d
Mark Saville <mark.saville@outlook.com>
2023-07-12 11:53:09 +0200

    create ui pattern to insert modal on click

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,7 @@ function addListItem(pokemon) { // adds list item to DOM
 
 function showDetails(pokemon) { // displays pokemon details
   loadDetails(pokemon).then(function() {
+    showModal(pokemon.name, 'Height: ' + pokemon.height + 'm' + '<br>' + 'Type: ' + pokemon.types[0].type.name);
     console.log(pokemon);
   });
 }
@@ -85,6 +86,54 @@ function hideLoadingMessage() { // hide loading message
   finishedLoading.classList.add('is-hidden');
 }
 
+
+let modalContainer = document.querySelector('#modal-container');
+function showModal(title, text) {
+  // Clear all existing modal content
+  
+  modalContainer.innerHTML = '';
+  
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+  
+  // Add the new modal content
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+  
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+  
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+  
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+  
+  modalContainer.classList.add('is-visible');
+}
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');  
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();  
+  }
+});
+
+modalContainer.addEventListener('click', (e) => {
+  // Since this is also triggered when clicking INSIDE the modal container,
+  // We only want to close if the user clicks directly on the overlay
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
 
 return {
   add: add,
